11d2b4207c853a69be20972c37799275e92e1939
jsanguinetti <jsangui@gmail.com>
2019-06-10 17:25:34 -0300

    improved pong function

--- features/challonge.js
+++ features/challonge.js
@@ -1,11 +1,23 @@
 const challonge = require("../commands/challonge");
 
+async function pong(bot) {
+  await bot.reply(message, "PONG :table_tennis_paddle_and_ball:");
+}
+
 module.exports = function(controller) {
   controller.on("slash_command", challonge);
 
   controller.on("direct_message", async (bot, message) => {
     if (message.text && message.text.toLowerCase() === "ping") {
-      await bot.reply(message, "PONG :table_tennis_paddle_and_ball:");
+      await pong(bot);
     }
   });
+
+  controller.hears(
+    async message => message.text && message.text.toLowerCase() === "ping",
+    ["message"],
+    async (bot, message) => {
+      await pong(bot);
+    }
+  );
 };
--- features/sample_echo.js
+++ features/sample_echo.js
@@ -4,10 +4,9 @@
  */
 
 module.exports = function(controller) {
-  controller.hears("sample", "message", async (bot, message) => {
-    await bot.reply(message, "I heard a sample message.");
-  });
-
+  // controller.hears("sample", "message", async (bot, message) => {
+  //   await bot.reply(message, "I heard a sample message.");
+  // });
   // controller.on('message', async(bot, message) => {
   //     await bot.reply(message, `Echo: ${ message.text }`);
   // });

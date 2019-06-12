f6b49f80a4ba6a0310f516e1997284d2485e2749
jsanguinetti <jsangui@gmail.com>
2019-06-12 16:36:59 -0300

    added more reactions

--- reactions/index.js
+++ reactions/index.js
@@ -1,10 +1,54 @@
 const reactions = message => {
+  let reactionRes = [];
   if (message.text) {
-    console.log("message.text.toLowerCase()", message.text.toLowerCase());
-    if (message.text.toLowerCase().includes("fiamene")) {
-      return ["femiane2", "smiling_femiane", "happy_fiamene"];
+    const lowerCaseMessage = message.text.toLowerCase();
+    if (
+      message.text.includes("U3FR8M5A7") ||
+      lowerCaseMessage.includes("fiamene")
+    ) {
+      reactionRes.push(
+        "femiane2",
+        "smiling_femiane",
+        "happy_fiamene",
+        "fiamante_bandido",
+        "femiane_ref",
+        "cop",
+        "church"
+      );
+    }
+    if (
+      message.text.includes("U326T7S66") ||
+      lowerCaseMessage.includes("colli") ||
+      lowerCaseMessage.includes("collazo")
+    ) {
+      reactionRes.push(
+        "scottie_pippen",
+        "happy_halloween",
+        "scottie_pippen_anim",
+        "sad_collazo"
+      );
+    }
+    if (
+      message.text.includes("U41F763DG") ||
+      lowerCaseMessage.includes("alfred")
+    ) {
+      reactionRes.push("happy-alfred", "eltres", "three");
+    }
+    if (
+      message.text.includes("U6345ABTM") ||
+      lowerCaseMessage.includes("matu") ||
+      lowerCaseMessage.includes("heredia")
+    ) {
+      reactionRes.push(
+        "heredia_sol",
+        "heredia_hamburglar",
+        "crazy_heredia",
+        "heredia_ahhhh",
+        "stealthy_heredia"
+      );
     }
   }
+  return reactionRes;
 };
 
 /** @param {import('botbuilder-adapter-slack').SlackBotWorker} bot */
@@ -30,10 +74,14 @@ const react = async (bot, message, reactionsToUse) => {
 module.exports = controller => {
   controller.on("direct_message", async (bot, message) => {
     const reactionsToUse = reactions(message);
-    if (!!reactionsToUse) {
+    if (reactionsToUse.length > 0) {
       await react(bot, message, reactionsToUse);
     }
   });
 
-  controller.hears(message => !!reactions(message), ["message"], react);
+  controller.hears(
+    message => reactions(message).length > 0,
+    ["message"],
+    react
+  );
 };

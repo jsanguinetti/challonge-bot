cfa5e7f497fd065b4b6dcfeb8b641de3c419aecb
jsanguinetti <jsangui@gmail.com>
2019-08-30 16:36:44 -0300

    agrega exception de ejemplo al bot

--- commands/challonge_dev/index.js
+++ commands/challonge_dev/index.js
@@ -1,1 +1,3 @@
-module.exports = async (bot, message) => {};
+module.exports = async (bot, message) => {
+    throw new Error('Este es un error de ejemplo para probar Stackdriver Error Reporting')
+};
